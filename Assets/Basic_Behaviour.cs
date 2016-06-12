c8a5eb682003a27d014a8c676d61925b1f5b52d2
Hyunjae Lee <guswoqkrtk@gmail.com>
2016-06-12 11:39:44 +0900

    First Commit

---
+++ Assets/Basic_Behaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basic_Behaviour : MonoBehaviour {
+
+	public int Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			transform.Translate (Vector3.forward*Speed*Time.deltaTime);
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (Vector3.left*Speed*Time.deltaTime);
+		if (Input.GetKey (KeyCode.S))
+			transform.Translate (Vector3.back*Speed*Time.deltaTime);
+		if (Input.GetKey (KeyCode.D))
+			transform.Translate (Vector3.right*Speed*Time.deltaTime);
+	}
+}
